6f91254ebe056fd2cec026609ea71ae0a9068d18
Kurt <kjensen8292@gmail.com>
2016-08-17 20:10:55 -0400

    Allow pfgeopoints.

--- Sources/ParseLiveQuery/Internal/QueryEncoder.swift
+++ Sources/ParseLiveQuery/Internal/QueryEncoder.swift
@@ -16,14 +16,39 @@ import Parse
 extension Dictionary where Key: StringLiteralConvertible, Value: AnyObject {
     init(query: PFQuery) {
         self.init()
-
+        
         let queryState = query.valueForKey("state")
         if let className = queryState?.valueForKey("parseClassName") {
             self["className"] = className as? Value
         }
-
+        
         if let conditions: [String:AnyObject] = queryState?.valueForKey("conditions") as? [String:AnyObject] {
-            self["where"] = conditions as? Value
+            let newConditions = conditions.sanitizeParseObjects()
+            self["where"] = newConditions as? Value
         }
+    }
+}
+
+extension Dictionary {
+    func sanitizeParseObjects() -> Dictionary {
+        var newSelf = Dictionary()
+        for keyVal in self {
+            if let dict = keyVal.1 as? [String:AnyObject] {
+                newSelf[keyVal.0] = dict.sanitizeParseObjects() as? Value
+            } else if let geoPoint = keyVal.1 as? PFGeoPoint {
+                newSelf[keyVal.0] = geoPoint.toDict() as? Value
+            } else {
+                newSelf[keyVal.0] = keyVal.1
+            }
+        }
+        return newSelf
+    }
+}
+
+extension PFGeoPoint {
+    func toDict() -> [String:AnyObject] {
+        return ["__type": "GeoPoint",
+                "latitude": latitude,
+                "longitude": longitude]
     }
 }
