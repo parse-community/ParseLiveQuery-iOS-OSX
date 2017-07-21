6e4e300550f1479814480bffd3153485f173d3c5
ananfang <ananfang@gmail.com>
2017-07-21 18:04:34 +0800

    WebSocket log will be printed in default. Set shouldPrintWebSocketLog to false to turn it off

--- Sources/ParseLiveQuery/Client.swift
+++ Sources/ParseLiveQuery/Client.swift
@@ -23,7 +23,7 @@ open class Client: NSObject {
     let clientKey: String?
 
     var socket: WebSocket?
-    var shouldPrintWebSocketLog = false
+    var shouldPrintWebSocketLog = true
     public var userDisconnected = false
 
     // This allows us to easily plug in another request ID generation scheme, or more easily change the request id type
