21d375babb6de59c3ed416411787b6d0fc37d475
Corey MacBook Pro <coreyearleon@icloud.com>
2020-11-15 12:57:21 -0500

    update jazzy script

---
+++ jazzy.sh
@@ -0,0 +1,13 @@
+ver=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" Sources/ParseLiveQuery/Info.plist`
+bundle exec jazzy \
+--clean \
+--author "Parse Community" \
+--author_url http://parseplatform.org \
+--github_url https://github.com/parse-community/ParseLiveQuery-iOS-OSX \
+--root-url http://parseplatform.org/ParseLiveQuery-iOS-OSX/ \
+--module-version ${ver} \
+--theme fullwidth \
+--skip-undocumented \
+--output ./docs \
+--build-tool-arguments -scheme,ParseLiveQuery-iOS \
+--module ParseLiveQuery \
